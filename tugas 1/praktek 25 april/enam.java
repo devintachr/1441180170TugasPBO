989ce4083171ac93a1a6a83c09cc7aa754b103c7
devintachr <dee17devinta.dc@gmail.com>
2015-03-27 10:10:26 -0700

    Tugas 2
    tugas tanggal 25 april

---
+++ tugas 1/praktek 25 april/enam.java
@@ -0,0 +1,13 @@
+public class enam {
+	public static void main(String[] args) {
+		int i = 0;
+		float jumlah = 0;
+		while (i < 8) {
+			i++;
+			jumlah +=i;
+
+		
+		}
+		System.out.print(+jumlah / i);
+	}
+}
---
+++ tugas 1/praktek 25 april/lima.java
@@ -0,0 +1,13 @@
+public class lima {
+	public static void main(String[] args) {
+		int i = 0;
+		int jumlah = 0;
+		while (i < 8) {
+			i++;
+			jumlah +=i;
+
+		
+		}
+		System.out.print(+jumlah);
+	}
+}
